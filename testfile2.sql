34cd8c9bfe1fce2b26df7ba193ace37332acdfe8
ADMIN <waw-dex-notifications-aaaaiwuw472ok7f7ch4oubogu4@snowflake.org.slack.com>
2025-05-09 17:40:46 +0000

    New branch "newbranch9" created from newbranch7

--- testfile2.sql
+++ testfile2.sql
@@ -1,1 +1,15 @@
 -- testfile2
+-- Set variables
+  set src_stage = 'my-remote-repo';
+  set src_path = 'branches/main/';
+  set tgt_stage = 'prf_stage';
+
+  -- Compose and execute the statement
+  -- Set variables
+set src_stage = 'my-remote-repo';
+set src_path = 'branches/main/';
+set tgt_stage = 'prf_stage';
+
+set sql_cmd = 'copy files into @' || $tgt_stage || ' from @' || $src_stage || '/' || $src_path || ';';
+
+execute immediate $sql_cmd;
