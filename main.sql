5523526363f1bf827e90d326bf86c52035d41d10
Robert Przystasz <robert.przystasz@snowflake.com>
2025-08-25 16:04:14 +0200

    Define database, schema, and tables for tasks
    Added database, schema, and table definitions for tasks.

--- main.sql
+++ main.sql
@@ -1,2 +1,4 @@
-DEFINE TASK {{ child_task }} WAREHOUSE = {{ wh }} COMMENT=c2 AFTER {{ parent_task }} AS SELECT 2;
-DEFINE TASK {{ parent_task }} WAREHOUSE = {{ wh }} COMMENT=c1 SCHEDULE = '{{ schedule }} MINUTE' AS SELECT 1;
+DEFINE DATABASE test_db;
+DEFINE SCHEMA test_db.test_schema;
+DEFINE TABLE test_db.test_schema.{{ child_task }};
+DEFINE TABLE test_db.test_schema.{{ parent_task}};
