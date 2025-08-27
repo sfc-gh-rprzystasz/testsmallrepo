0e4398592a9dd37bc27ac93f48ccabb79954c0e0
Robert Przystasz <robert.przystasz@snowflake.com>
2025-08-27 08:56:21 +0200

    Add columns to child_task and parent_task tables

--- main.sql
+++ main.sql
@@ -1,4 +1,4 @@
 DEFINE DATABASE test_db;
 DEFINE SCHEMA test_db.test_schema;
-DEFINE TABLE test_db.test_schema.{{ child_task }};
-DEFINE TABLE test_db.test_schema.{{ parent_task}};
+DEFINE TABLE test_db.test_schema.{{ child_task }} ( a INTEGER );
+DEFINE TABLE test_db.test_schema.{{ parent_task}} ( b INTEGER );
